20faa4e184bfe3deb07c6c5a4a7069dee21ff4eb
haa-zzz <2101297547@qq.com>
2020-10-30 09:35:11 +0800

    测试

--- my/src/main/java/com/littlecorgi/my/MyMainActivity.java
+++ my/src/main/java/com/littlecorgi/my/MyMainActivity.java
@@ -10,5 +10,6 @@ public class MyMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_main);
+        Log.d("Mainctiviy","123");
     }
 }
